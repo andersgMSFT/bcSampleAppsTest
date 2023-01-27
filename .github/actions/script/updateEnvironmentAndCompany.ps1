4a024c31847a925dce1a1f23ad5ee811359fb190
Anders Nørskov <andersg@microsoft.com>
2023-01-27 16:45:39 +0100

    improve update environmentAndCompany script

--- .github/actions/script/updateEnvironmentAndCompany.ps1
+++ .github/actions/script/updateEnvironmentAndCompany.ps1
@@ -6,40 +6,29 @@ param(
 
 function getCurrentSettings {
     $connectionFiles = Get-ChildItem -Recurse -File -Include "Connections.json";    
+    $currentSettingsList = @();
+
     foreach ($connectionFile in $connectionFiles) {
         $connectionsFilePath = $connectionFile.FullName;
-        $connectionsfile = (Get-ChildItem $connectionsFilePath);
-        
-        if ($connectionsfile.Exists) {
-            try {
-                $jsonFile = Get-Content $connectionsfile.FullName | ConvertFrom-Json;
-                $ConnectorNodeNames = ($jsonFile | Get-Member -MemberType NoteProperty).Name;
+        $jsonFile = Get-Content $connectionsFilePath | ConvertFrom-Json;
+        $ConnectorNodeNames = ($jsonFile | Get-Member -MemberType NoteProperty).Name;               
 
-                # We don't know the name of the connector node, so we need to loop through all of them
-                foreach($connectorNodeName in $ConnectorNodeNames){
-                    $connectorNode = $jsonFile.$connectorNodeName;
-                    if($connectorNode.connectionRef.displayName -eq "Dynamics 365 Business Central"){
-                        $currentEnvironmentAndCompany = ($connectorNode.datasets | Get-Member -MemberType NoteProperty).Name;
-                        return $currentEnvironmentAndCompany;     
-                    }
-                }
+        # We don't know the name of the connector node, so we need to loop through all of them
+        foreach ($connectorNodeName in $ConnectorNodeNames) {
+            $connectorNode = $jsonFile.$connectorNodeName;
+            if ($connectorNode.connectionRef.displayName -eq "Dynamics 365 Business Central") {
+                $currentEnvironmentAndCompany = ($connectorNode.datasets | Get-Member -MemberType NoteProperty).Name;
 
+                if (!$currentsettingsList.Contains($currentEnvironmentAndCompany)) {
+                    $currentSettingsList += $currentEnvironmentAndCompany;
+                } 
+                break;     
             }
-            catch {
-                Write-Error "Could not find connector node in file: " + $connectorId;
-                return "";
-            }
-            
-        }
-        else {
-            Write-Error "Could not find file: " + $connectionsfile;
-            return "";
         }
     }
-    Write-Error "No connection files in the current folder: ";
-    return "";
+    
+    return $currentSettingsList;
 }
-
 
 function replaceOldSettings {
     param(
@@ -50,23 +39,21 @@ function replaceOldSettings {
 
     $powerAppFiles = Get-ChildItem -Recurse -File $rootFolder
     foreach ($file in $powerAppFiles) {
-        $fileContent = Get-Content $file.FullName;
-        if (Select-String -Pattern $oldSetting -InputObject $fileContent) {
-            Set-Content -Path $file.FullName -Value $fileContent.Replace($oldSetting, $newSetting);
-            Write-Host $file.FullName" --> updated ";
+        # only check json and xml files
+        if (($file.Extension -eq ".json") -or ($file.Extension -eq ".xml")) {
+            
+            $fileContent = Get-Content $file.FullName;
+            if (Select-String -Pattern $oldSetting -InputObject $fileContent) {
+                Set-Content -Path $file.FullName -Value $fileContent.Replace($oldSetting, $newSetting);
+                Write-Host $file.FullName" <-- updated ";
+            }
         }
     }
-
 }
 
 $currentSettings = getCurrentSettings;
-if ([string]::IsNullOrEmpty($currentSettings)) {
+if ($currentSettings.Count -eq 0) {
     Write-Error "Could not find connections file";
-    return 2;
-}
-
-if ([string]::IsNullOrEmpty($companyId) -or [string]::IsNullOrEmpty($environmentName)) {
-    Write-Error "Missing environment or company"
     return 2;
 }
 
@@ -74,4 +61,11 @@ $newSettings = "$environmentName,$companyId";
 Write-Host "Current settings: "$currentSettings;
 Write-Host "New settings: "$newSettings;
 
-replaceOldSettings -oldSetting $currentSettings -newSetting $newSettings -rootFolder .;
+foreach ($currentSetting in $currentSettings) {
+    if ($currentSetting -eq $newSettings) {
+        Write-Host "No changes needed for: "$currentSetting;
+        continue;
+    }
+    Write-Host "Updating: "$currentSetting;
+    replaceOldSettings -oldSetting $currentSetting -newSetting $newSettings -rootFolder .;
+}
