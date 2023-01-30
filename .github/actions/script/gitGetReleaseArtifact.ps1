d856ca2b447ff6daa420b3190d74cb516076b22d
Anders Nørskov <andersg@microsoft.com>
2023-01-30 21:11:39 +0100

    update script

--- .github/actions/script/gitGetReleaseArtifact.ps1
+++ .github/actions/script/gitGetReleaseArtifact.ps1
@@ -1,13 +1,13 @@
 param(
-    [string]$repo,
-    [string]$owner
+    [string]$repo = "bcSampleAppsTest",
+    [string]$owner = "andersgMSFT"
 )
 
 Write-Host "Getting latest release from GitHub";
 $response = Invoke-WebRequest -Uri https://api.github.com/repos/$owner/$repo/releases/latest
 
 Write-Host "Getting download URL for solutions.zip";
-$downloadUrl = ($response.Content | ConvertFrom-Json).assets | Where-Object { $_.name -eq "solutions.zip" } | Select-Object -ExpandProperty browser_download_url
+$downloadUrl = ($response.Content | ConvertFrom-Json).assets | Where-Object { $_.name -eq "BcSampleAppsSolution_0.0.0.1.zip" } | Select-Object -ExpandProperty browser_download_url
 
-write-host "Downloading solutions.zip";
+write-host "Downloading: " $downloadUrl;
 Invoke-WebRequest -Uri $downloadUrl -OutFile solutions.zip
